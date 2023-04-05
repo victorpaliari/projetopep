4981210f1234a50b0376264a7255e8f3a53ee7e2
Victor Paliari <victorrpaliari@gmail.com>
2023-04-05 16:30:49 -0300

    Tentando colocar logo no meio do header.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-window.addEventListener('scroll', function() {
+/* window.addEventListener('scroll', function() {
     var scrollPosition = window.pageYOffset;
     var parallaxElement = document.querySelector('.parallax');
     parallaxElement.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
-  });
+  }); */
+  
